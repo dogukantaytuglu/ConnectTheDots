425631b4c125b17c5bff5d183c0d8629e88bde58
dogukantayuglu <dogukantaytuglu@gmail.com>
2024-03-11 19:35:57 +0100

    Tidy up grid settings

--- Assets/Game/Features/Grid/Scripts/Settings/GridSettings.cs
+++ Assets/Game/Features/Grid/Scripts/Settings/GridSettings.cs
@@ -16,15 +16,17 @@ namespace Game.Features.Grid.Scripts.Settings
         public GridCellEntity GridCellEntityPrefab => gridCellEntityPrefab;
         public float HorizontalSpaceBetweenCells => horizontalSpaceBetweenCells;
         public float VerticalSpaceBetweenCells => verticalSpaceBetweenCells;
-
+        
+        [Header("Grid Shape Properties")]
         [SerializeField] private int horizontalGridSize;
         [SerializeField] private int verticalGridSize;
-        [SerializeField] private bool isDebugViewActive;
-        [SerializeField] private float cellScale;
-        [SerializeField] private float topMargin;
-        [SerializeField] private string parentName;
-        [SerializeField] private GridCellEntity gridCellEntityPrefab;
         [SerializeField] private float horizontalSpaceBetweenCells;
         [SerializeField] private float verticalSpaceBetweenCells;
+        [SerializeField] private float cellScale;
+        [SerializeField] private float topMargin;
+        [Header("Other Properties")]
+        [SerializeField] private string parentName;
+        [SerializeField] private GridCellEntity gridCellEntityPrefab;
+        [SerializeField] private bool isDebugViewActive;
     }
 }
