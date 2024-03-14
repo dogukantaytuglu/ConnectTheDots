71523491dd3b0a0561fff45c7a7bf3d28b800029
dogukantayuglu <dogukantaytuglu@gmail.com>
2024-03-14 21:17:31 +0100

    Remove redundant installer

--- Assets/Game/Features/TargetValueReferenceDot/Installer/TargetValueReferenceDotInstaller.cs
+++
@@ -1,9 +0,0 @@
-using Zenject;
-
-namespace Game.Features.TargetValueReferenceDot.Installer
-{
-    public class TargetValueReferenceDotInstaller : MonoInstaller<TargetValueReferenceDotInstaller>
-    {
-
-    }
-}
